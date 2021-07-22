f4fd6afd42e91e3388a35ad1857bd37eb5992a27
infmatematicas <87808011+infmatematicas@users.noreply.github.com>
2021-07-22 13:28:04 +0200

    Update jupyterhub_config.py

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -16,20 +16,16 @@ c.JupyterHub.admin_access = True
 c.Spawner.default_url = '/lab'
 
 ## Authenticator
-from jhub_cas_authenticator.cas_auth import CASAuthenticator
-c.JupyterHub.authenticator_class = CASAuthenticator
+c.JupyterHub.authenticator_class = 'ldapauthenticator.LDAPAuthenticator'
+c.LDAPAuthenticator.server_address='127.0.0.1'
+c.LDAPAuthenticator.bind_dn_template='cn={username},dc=mat,dc=localnet'
+c.LDAPAuthenticator.server_port=389
+c.LDAPAuthenticator.use_ssl=False
+c.LDAPAuthenticator.user_search_base='dc=mat,dc=localnet'
+c.LDAPAuthenticator.user_attribute='cn'
 
-# The CAS URLs to redirect (un)authenticated users to.
-c.CASAuthenticator.cas_login_url = 'https://cas.uvsq.fr/login'
-c.CASLocalAuthenticator.cas_logout_url = 'https://cas.uvsq/logout'
 
-# The CAS endpoint for validating service tickets.
-c.CASAuthenticator.cas_service_validate_url = 'https://cas.uvsq.fr/serviceValidate'
-
-# The service URL the CAS server will redirect the browser back to on successful authentication.
-c.CASAuthenticator.cas_service_url = 'https://%s/hub/login' % os.environ['HOST']
-
-c.Authenticator.admin_users = { 'lucadefe' }
+c.Authenticator.admin_users = { 'amp-jupyter' }
 
 
 ## Docker spawner
@@ -41,14 +37,16 @@ c.JupyterHub.hub_ip = os.environ['HUB_IP']
 
 # user data persistence
 # see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
-notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/administrador'
 c.DockerSpawner.notebook_dir = notebook_dir
 c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
 
 # Other stuff
 c.Spawner.cpu_limit = 1
-c.Spawner.mem_limit = '10G'
+c.Spawner.mem_limit = '2G'
 
+# EXTRA
+c.JupyterHub.subdomain_host = 'https://jupyt3r.mat.uam.es'
 
 ## Services
 c.JupyterHub.services = [
